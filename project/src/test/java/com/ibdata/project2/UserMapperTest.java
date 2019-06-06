d6f1e18a55599e9d651296eafbabdd5432739c88
ixtears23 <ixtears23@icloud.com>
2019-06-07 01:31:02 +0900

    DBUnit 적용중

---
+++ project/src/test/java/com/ibdata/project2/UserMapperTest.java
@@ -0,0 +1,41 @@
+package com.ibdata.project2;
+
+import org.dbunit.DBTestCase;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value = { "classpath:/config/*-context.xml" })
+@TestExecutionListeners({ 
+	DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class
+//        DbUnitTestExecutionListener.class 
+        })
+@DbUnitConfiguration(databaseConnection={"datasource"})	// 기본적으로 dataSource 이름을 찾는다. camel표기법인데 난 전부 소문자로 bean 이름을 등록해서 해당 설정 추가함.
+public class UserMapperTest {
+	
+	@Test
+//	@DatabaseSetup
+//	@ExpectedDatabase
+	@DatabaseSetup("classpath:com/ibdata/project2/mapper/userCopy.xml")
+	@ExpectedDatabase("classpath:com/ibdata/project2/mapper/userCopy.xml")
+	public void dataTest() {
+		
+	}
+
+
+}
